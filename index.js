99df758317f36202fba50331ef2895c295df66cf
Elieke Schuurbiers <Elieke@schuurbiers.com>
2020-03-26 19:11:21 +0100

    Dom Manipulations

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+const buttonClicked = document.querySelectorAll(".big-five-button");
+buttonClicked.forEach(function (button) {
+    button.addEventListener("click", function (c) {
+        const newLi = document.createElement("li")
+        const list = document.getElementById("spotted-animals-list")
+        list.appendChild(newLi);
+
+        newLi.innerHTML = c.toElement.innerHTML;
+
+    });
+
+});
+
+const removeFirstitem = document.getElementById("remove-first-item-button");
+removeFirstitem.addEventListener("click", function () {
+    const list = document.getElementById("spotted-animals-list")
+    list.firstElementChild.remove();
+    // alert("boo")
+});
+
+const removeAll = document.getElementById("remove-all-button");
+removeAll.addEventListener("click", function () {
+    const list = document.getElementById("spotted-animals-list")
+    while (list.hasChildNodes) {
+        list.firstChild.remove()
+    };
+
+
+});
+
+while (i < colors.length) {
+    console.log(colors[i])
+    i++
+
+}
